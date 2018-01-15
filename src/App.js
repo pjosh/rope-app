b6bf26319a19ca5b5bcb8efe085fafb46d80da02
pjosh <jorgegar@gmail.com>
2018-01-15 23:18:09 +0100

    project settings

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/styles.scss';
+import styles from './App.scss';
+import logoIcon from './assets/icons/logo.svg';
+
+import Icon from './components/icon';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className={styles.App}>
+        <header className={styles.header}>
+          <Icon icon={logoIcon} className={styles.logo} />
+          <h1 className={styles.title}>Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <p className={styles.intro}>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
